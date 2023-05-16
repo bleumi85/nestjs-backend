f90b1810ec83651d0c575ae51a5a835b75784c24
Jens Bleumer <jens.bleumer@gmail.com>
2023-05-16 09:04:46 +0200

    jbl(): changes on env

--- src/config/validation.ts
+++ src/config/validation.ts
@@ -6,4 +6,10 @@ export const validationSchema = Joi.object({
         .valid(NODE_ENV.PRODUCTION, NODE_ENV.DEVELOPMENT, NODE_ENV.TEST)
         .default(NODE_ENV.DEVELOPMENT),
     PORT: Joi.number().default(3005),
+    POSTGRES_HOST: Joi.string().required(),
+    POSTGRES_USER: Joi.string().required(),
+    POSTGRES_PASSWORD: Joi.string().required().allow(''),
+    POSTGRES_DB: Joi.string().required(),
+    JWT_SECRET: Joi.string().required(),
+    JWT_EXPIRES_IN: Joi.string().required(),
 });
